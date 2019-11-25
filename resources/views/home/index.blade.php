587286fb00d627eb3eb748b0087b4dcb66d635a6
Unknown <jeffry24797@gmail.com>
2019-11-25 12:40:01 +0700

    Form update

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -66,13 +66,13 @@
 
     <div class="wave">
         <div class="wave1" >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1350 402"><defs><style>.cls-1{fill:#fefefe;}</style></defs><title>wave</title><g id="Layer_2" data-name="Layer 2"><g id="OBJECTS"><path class="cls-1" d="M1350,402H0V377.23a451.76,451.76,0,0,1,32-33.42,45.08,45.08,0,0,1,3.55-3.9c14.41-13.43,31-24,48.62-33.12C129.29,283.46,178,271,228.67,264.94c25.3-3,50.69-4.32,76.19-4.93,15.65-.37,31.31-1,46.94-.57,26.45.78,52.93.79,79.37,1.92,22.62,1,45.25,1.4,67.88,1.93,18.48.43,37,.83,55.44.77,44.88-.15,89.52-3.36,133.73-10.77,29.81-5,58.45-14.09,86.71-24.21,45.67-16.34,89-37.42,131.55-59.91,43.44-22.94,85.77-47.69,129.18-70.66,53.56-28.32,108.28-54.19,166.81-72A640.25,640.25,0,0,1,1325,2c6.14-.62,22-2,25-2"/></g></g></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1350 402"><defs><style>.cls-1{fill:#fefefe;}</style></defs><title>ByteForce</title><g id="Layer_2" data-name="Layer 2"><g id="OBJECTS"><path class="cls-1" d="M1350,402H0V377.23a451.76,451.76,0,0,1,32-33.42,45.08,45.08,0,0,1,3.55-3.9c14.41-13.43,31-24,48.62-33.12C129.29,283.46,178,271,228.67,264.94c25.3-3,50.69-4.32,76.19-4.93,15.65-.37,31.31-1,46.94-.57,26.45.78,52.93.79,79.37,1.92,22.62,1,45.25,1.4,67.88,1.93,18.48.43,37,.83,55.44.77,44.88-.15,89.52-3.36,133.73-10.77,29.81-5,58.45-14.09,86.71-24.21,45.67-16.34,89-37.42,131.55-59.91,43.44-22.94,85.77-47.69,129.18-70.66,53.56-28.32,108.28-54.19,166.81-72A640.25,640.25,0,0,1,1325,2c6.14-.62,22-2,25-2"/></g></g></svg>
         </div>
         <div class="wave2" >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1313.88 477.2"><defs><style>.cls-1{fill:#fff;}</style></defs><title>wave</title><g id="Layer_2" data-name="Layer 2"><g id="OBJECTS"><path class="cls-1" d="M0,477.2c7.55-11.13,17.62-20,27.4-28.93,22.27-20.39,47.78-36.09,75-49,35.4-16.74,72.62-27.84,111-35.37a717.66,717.66,0,0,1,72-10.13c21.84-2,43.64-4.07,65.57-5,15.78-.69,31.55-1.47,47.35-1.91q39.39-1.11,78.77-3.06c14.11-.71,28.26-1,42.33-2,22.52-1.69,45.06-3,67.51-5.62,26.58-3.06,53.05-6.74,79.22-12.24,44.06-9.25,85.82-25,126.34-44.53,37.44-18,73-39.27,108-61.46,46.86-29.67,91.9-62,138.29-92.43,33.29-21.81,67.18-42.61,102.38-61.29a723.47,723.47,0,0,1,110.91-47.68A560.09,560.09,0,0,1,1313.87,0c0,2,0,113.5,0,121-81.46,5.23-158.52,27-232.21,61.58C1024.59,209.4,970.43,241.49,916,273.15c-53.87,31.33-108.31,61.57-166.46,84.48-31,12.22-62.56,22.89-95.44,28.88a694.7,694.7,0,0,1-94.24,10.35c-63.32,2.76-126.57-.54-189.84-2.43-50.63-1.52-101.29-2.67-151.83,1.77S118.08,410.6,71.44,431.92c-25,11.43-48.3,25.45-69,43.66A21.77,21.77,0,0,1,0,477.2Z"/></g></g></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1313.88 477.2"><defs><style>.cls-1{fill:#fff;}</style></defs><title>ByteForce</title><g id="Layer_2" data-name="Layer 2"><g id="OBJECTS"><path class="cls-1" d="M0,477.2c7.55-11.13,17.62-20,27.4-28.93,22.27-20.39,47.78-36.09,75-49,35.4-16.74,72.62-27.84,111-35.37a717.66,717.66,0,0,1,72-10.13c21.84-2,43.64-4.07,65.57-5,15.78-.69,31.55-1.47,47.35-1.91q39.39-1.11,78.77-3.06c14.11-.71,28.26-1,42.33-2,22.52-1.69,45.06-3,67.51-5.62,26.58-3.06,53.05-6.74,79.22-12.24,44.06-9.25,85.82-25,126.34-44.53,37.44-18,73-39.27,108-61.46,46.86-29.67,91.9-62,138.29-92.43,33.29-21.81,67.18-42.61,102.38-61.29a723.47,723.47,0,0,1,110.91-47.68A560.09,560.09,0,0,1,1313.87,0c0,2,0,113.5,0,121-81.46,5.23-158.52,27-232.21,61.58C1024.59,209.4,970.43,241.49,916,273.15c-53.87,31.33-108.31,61.57-166.46,84.48-31,12.22-62.56,22.89-95.44,28.88a694.7,694.7,0,0,1-94.24,10.35c-63.32,2.76-126.57-.54-189.84-2.43-50.63-1.52-101.29-2.67-151.83,1.77S118.08,410.6,71.44,431.92c-25,11.43-48.3,25.45-69,43.66A21.77,21.77,0,0,1,0,477.2Z"/></g></g></svg>
         </div>
         <div class="wave3">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1312 567"><defs><style>.cls-1{fill:#fff;}</style></defs><title>wave</title><g id="Layer_2" data-name="Layer 2"><g id="OBJECTS"><path class="cls-1" d="M1312,0V94c-5,1-90,19-165.55,60.29-48.51,26.51-96.49,56-142.67,87.45-55.24,37.58-110.14,75.69-168.53,108.38-37.43,21-76.14,39.23-116.83,52.93-34.85,11.74-70.81,18.81-107.23,23.63-24.11,3.19-48.28,5.66-72.56,7.22-18.43,1.2-36.88,2.09-55.33,3-25.6,1.22-51.21,2.1-76.82,3-22.78.82-45.59,1.36-68.32,2.95-53.53,3.74-107,8.46-159.11,22.2C133.78,477,90.85,494.26,52.23,521.25A222.21,222.21,0,0,0,21.06,547c-7.49,7.57-20,19.12-21.06,20,6.23-12.42,9.76-18.3,18.51-29,18.63-22.69,40.67-41.55,65.26-57.45,32.65-21.11,68-36.4,105.07-47.8,29.45-9.05,59.49-15.48,89.84-20.39,18-2.91,36.12-5.15,54.23-7.38,17.44-2.15,34.92-4,52.41-5.78C398,398,410.78,397.13,423.51,396c16.84-1.52,33.68-3.14,50.52-4.76,10.73-1,21.44-2.29,32.19-3.15,18.53-1.49,37-3.76,55.36-6.33C581.29,379,601,376.05,620.5,372c34.4-7.1,68.44-15.52,101.16-28.49,36-14.27,70-32.62,103-52.66,41.73-25.31,81.06-54.12,120-83.44,38.65-29.1,77-58.61,116.42-86.7a964.41,964.41,0,0,1,90.68-57.29,671.85,671.85,0,0,1,125-53.78C1288.32,6,1300.3,3.21,1312,0Z"/></g></g></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1312 567"><defs><style>.cls-1{fill:#fff;}</style></defs><title>ByteForce</title><g id="Layer_2" data-name="Layer 2"><g id="OBJECTS"><path class="cls-1" d="M1312,0V94c-5,1-90,19-165.55,60.29-48.51,26.51-96.49,56-142.67,87.45-55.24,37.58-110.14,75.69-168.53,108.38-37.43,21-76.14,39.23-116.83,52.93-34.85,11.74-70.81,18.81-107.23,23.63-24.11,3.19-48.28,5.66-72.56,7.22-18.43,1.2-36.88,2.09-55.33,3-25.6,1.22-51.21,2.1-76.82,3-22.78.82-45.59,1.36-68.32,2.95-53.53,3.74-107,8.46-159.11,22.2C133.78,477,90.85,494.26,52.23,521.25A222.21,222.21,0,0,0,21.06,547c-7.49,7.57-20,19.12-21.06,20,6.23-12.42,9.76-18.3,18.51-29,18.63-22.69,40.67-41.55,65.26-57.45,32.65-21.11,68-36.4,105.07-47.8,29.45-9.05,59.49-15.48,89.84-20.39,18-2.91,36.12-5.15,54.23-7.38,17.44-2.15,34.92-4,52.41-5.78C398,398,410.78,397.13,423.51,396c16.84-1.52,33.68-3.14,50.52-4.76,10.73-1,21.44-2.29,32.19-3.15,18.53-1.49,37-3.76,55.36-6.33C581.29,379,601,376.05,620.5,372c34.4-7.1,68.44-15.52,101.16-28.49,36-14.27,70-32.62,103-52.66,41.73-25.31,81.06-54.12,120-83.44,38.65-29.1,77-58.61,116.42-86.7a964.41,964.41,0,0,1,90.68-57.29,671.85,671.85,0,0,1,125-53.78C1288.32,6,1300.3,3.21,1312,0Z"/></g></g></svg>
         </div>
         <div class="img-phone">
             <img src="{{ asset('images/phone.svg') }}" alt="technology web apps">
@@ -240,13 +240,16 @@
                             <div class="form-group">
                                 <label>Email</label>
                                 <input id="contactEmail" type="email" class="form-control" placeholder="Enter email">
-                                <small class="form-text text-muted">We dont share your email to anyone.</small>
+                                <small class="form-text text-muted">We don't share your email to anyone.</small>
+
                             </div>
                         </div>
                     </div>
+                    <small id="messageError" class="form-text text-danger" >Please complete the form.</small>
                     <div class="send-wrapper">
                         <input type="button" class="bttn" id="btnSendContactUs" value="Send">
                     </div>
+
                 </div>
             </div>
         </div>
@@ -270,44 +273,62 @@
             }
         });
 
+        var messageError = $("#messageError");
+        messageError.css("visibility","hidden");
+
         $("#btnmodal").click(function(){
             $(".modal").css("display","block");
         });
         $(".close").click(function(){
             $(".modal").css("display","none");
+            messageError.css("visibility","hidden");
         });
         $(window).click(function(e){
             if($(e.target).is(".modal")){
                 $(".modal").css("display","none");
+                messageError.css("visibility","hidden");
             }
         });
         $("#btnSendContactUs").click(function (e) {
-            var button = $(this);
-            button.val("Sending...");
-            button.attr("disabled", true);
 
-            $.ajax({
-                type: "POST",
-                contentType: 'application/json',
-                url: "/contact-us/new",
-                dataType: "json",
-                data: JSON.stringify({
-                    "contactName": $("#contactName").val(),
-                    "contactEmail": $("#contactEmail").val(),
-                    "contactMessage": $("#contactMessage").val()
-                }),
-                success: function (json) {
-                    console.log("success");
-                    button.val("Send");
-                    button.attr("disabled", false);
-                },
-                error: function (xhr, status, error) {
-                    console.log("error, status code: "+ xhr.status);
-                    console.log(xhr.responseText);
-                    button.val("Send");
-                    button.attr("disabled", false);
-                }
-            });
+            var contactName = $("#contactName").val();
+            var contactEmail = $("#contactEmail").val();
+            var contactMessage = $("#contactMessage").val();
+
+            if (!contactName || !contactEmail || !contactMessage) {
+                messageError.text("Please complete the form.");
+                messageError.css("visibility","visible");
+            } else {
+                messageError.css("visibility","hidden");
+                var button = $(this);
+                button.val("Sending...");
+                button.attr("disabled", true);
+
+                $.ajax({
+                    type: "POST",
+                    contentType: 'application/json',
+                    url: "/contact-us/new",
+                    dataType: "json",
+                    data: JSON.stringify({
+                        "contactName": contactName,
+                        "contactEmail": contactEmail,
+                        "contactMessage": contactMessage
+                    }),
+                    success: function (json) {
+                        console.log("success");
+                        button.val("Sent");
+                        button.attr("disabled", true);
+                    },
+                    error: function (xhr, status, error) {
+                        messageError.css("visibility","visible");
+                        messageError.text("Something went wrong. Please try again later.");
+                        console.log("error, status code: "+ xhr.status);
+                        console.log(xhr.responseText);
+                        button.val("Send");
+                        button.attr("disabled", false);
+                    }
+                });
+            }
         });
     });
 
