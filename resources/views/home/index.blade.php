b6eb0135abd46e7ec1ab4f8ee4efa0e6aeb81109
David Adrias <david.suryaatmaja@binus.ac.id>
2019-11-25 15:32:25 +0700

    fix wave bug v2

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -334,11 +334,6 @@
         });
     });
 
-    window.addEventListener('resize', () => {
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
-
 </script>
 </body>
 </html>
