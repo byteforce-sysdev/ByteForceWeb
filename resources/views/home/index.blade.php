f5324c191eb91b602dc7fe91701be10d854cf18b
Unknown <jeffry24797@gmail.com>
2019-11-26 16:28:15 +0700

    Separated JS

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -38,6 +38,7 @@
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <!-- <script src="https://unpkg.com/popper.js"></script> -->
 
+    <script src="{{ asset('js/home.js') }}"></script>
 </head>
 
 <body>
@@ -287,97 +288,5 @@
 
 </div>
 
-<script>
-    $(document).ready(function () {
-        AOS.init(
-            {
-                duration: 500,
-                easing:'ease-in-out',
-                once:true
-            }
-        );
-
-        var fixedButton = $("#fixedButton");
-        var scrolledPastLanding = false;
-
-        $(document).on('scroll', function() {
-            if($(this).scrollTop() >= $('#our-solutions').position().top){
-                fixedButton.css("visibility", "visible");
-                fixedButton.css("opacity", 1);
-                scrolledPastLanding = true;
-            } else if ($(this).scrollTop() < $('#our-solutions').position().top && scrolledPastLanding){
-                scrolledPastLanding = false;
-                fixedButton.css("opacity", 0);
-                setTimeout(function() {
-                    fixedButton.css("visibility", "hidden");
-                }, 300);
-            }
-        });
-
-        $.ajaxSetup({
-            headers: {
-                "X-CSRF_TOKEN": $('meta[name="csrf-token"]').attr("content")
-            }
-        });
-
-        var messageError = $("#messageError");
-        messageError.css("visibility","hidden");
-
-        $(".btnmodal").click(function(){
-            $(".modal").css("display","block");
-        });
-        $(".close").click(function(){
-            $(".modal").css("display","none");
-            messageError.css("visibility","hidden");
-        });
-        $(window).click(function(e){
-            if($(e.target).is(".modal")){
-                $(".modal").css("display","none");
-                messageError.css("visibility","hidden");
-            }
-        });
-        $("#btnSendContactUs").click(function (e) {
-
-            var contactName = $("#contactName").val();
-            var contactEmail = $("#contactEmail").val();
-            var contactMessage = $("#contactMessage").val();
-
-            if (!contactName || !contactEmail || !contactMessage) {
-                messageError.text("Please complete the form.");
-                messageError.css("visibility","visible");
-            } else {
-                messageError.css("visibility","hidden");
-                var button = $(this);
-                button.val("Sending...");
-                button.attr("disabled", true);
-
-                $.ajax({
-                    type: "POST",
-                    contentType: 'application/json',
-                    url: "/contact-us/new",
-                    dataType: "json",
-                    data: JSON.stringify({
-                        "_token": $('meta[name="csrf-token"]').attr("content"),
-                        "contactName": contactName,
-                        "contactEmail": contactEmail,
-                        "contactMessage": contactMessage
-                    }),
-                    success: function (json) {
-                        button.val("Sent");
-                        button.attr("disabled", true);
-                    },
-                    error: function (xhr, status, error) {
-                        messageError.css("visibility","visible");
-                        messageError.text("Something went wrong. Please try again later.");
-                        button.val("Send");
-                        button.attr("disabled", false);
-                    }
-                });
-            }
-        });
-
-    });
-
-</script>
 </body>
 </html>
