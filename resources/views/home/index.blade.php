341fdce2d5ddc44964ff8e52cf214c8a050b3c13
David Adrias <david.suryaatmaja@binus.ac.id>
2019-11-28 16:20:35 +0700

    add google analytics

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-153550735-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-153550735-1');
+    </script>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
